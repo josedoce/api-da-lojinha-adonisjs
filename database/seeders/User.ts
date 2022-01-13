9d38c7868f90b298c660df1bf70ff4cdaa9d398e
JAS <66434808+josedoce@users.noreply.github.com>
2022-01-12 23:03:23 -0300

    hashing na seed

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,5 +1,6 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import User from 'App/Models/User'
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+import Hash from '@ioc:Adonis/Core/Hash';
+import User from 'App/Models/User';
 
 interface IUser {
   name: string;
@@ -13,31 +14,41 @@ interface IUser {
 export default class UserSeeder extends BaseSeeder {
   public static developmentOnly = true
   public async run () {
+    const defaultPassword = '12345678';
     const users: IUser[] = [
       {
         name: 'usuario',
         email: 'usuario@gmail.com',
-        password: '$bcrypt$v=98$r=10$VGZOOEk7ZkPuNAY4QEvcVg$NBI4gTG8QA7OZL+zn1VXFO5c92fCIZQ',
+        password: '',
       },
       {
         name: 'cliente',
         email: 'cliente@gmail.com',
-        password: '$bcrypt$v=98$r=10$VGZOOEk7ZkPuNAY4QEvcVg$NBI4gTG8QA7OZL+zn1VXFO5c92fCIZQ',
+        password: '',
         is_client: true,
       },
       {
         name: 'vendedor',
         email: 'vendedor@gmail.com',
-        password: '$bcrypt$v=98$r=10$VGZOOEk7ZkPuNAY4QEvcVg$NBI4gTG8QA7OZL+zn1VXFO5c92fCIZQ',
+        password: '',
         is_seller: true,
       },
       {
         name: 'administrador',
         email: 'administrador@gmail.com',
-        password: '$bcrypt$v=98$r=10$VGZOOEk7ZkPuNAY4QEvcVg$NBI4gTG8QA7OZL+zn1VXFO5c92fCIZQ',
+        password: '',
         is_admin: true,
       },
     ];
-    await User.createMany(users);
+    const userWithHashInPassword = await Promise.all(
+      users.map(
+        async (obj: IUser)=>{
+          obj.password = await Hash.make(defaultPassword);
+          return obj;
+        }
+      )
+    );
+    
+    await User.createMany(userWithHashInPassword);
   }
 }
